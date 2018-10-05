7193c7e78750926ed935b21aa739522ad94a0156
Laquanta Taylor <laquantat28@yahoo.com>
2018-10-05 13:48:26 -0400

    Add new picture of kitten.

---
+++ index.js
@@ -0,0 +1,8 @@
+function validateLunch(){
+    var food = doucment.getElementById("lunchWeAte").value;
+    if(food === "gyro"){
+        alert("My stomach is full.");
+    }else{
+        alert("I'm still hungry!");
+    }
+}
